b21da6585795becd25f080f9104506ed53b27a70
Michael Mostachetti <mjmostachetti@gmail.com>
2015-05-11 16:39:21 -0700

    Made whatsAroundYou more concise.

--- game/game.js
+++ game/game.js
@@ -9,18 +9,6 @@ var rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 })
-/*
-prompt.start();
-
-prompt.get(['name'],function(err,result){
-	console.log('Your name is : ' + result.name)
-})
-
-map.rooms.[]
-
-*/
-//console.log(map.rooms)
-
 
 var currentRoom = map.rooms[0];
 
@@ -29,45 +17,32 @@ console.log("You are in room " + currentRoom.name);
 function whatsAroundYou(currentRoom){
 	var numMoves;
 	var directions = []
-	for(var x = 0; x < map.rooms.length; x++){
-		if(currentRoom.name === map.rooms[x].north){
-			directions.push('south')
-			numMoves++;
-		}
-		if(currentRoom.name === map.rooms[x].south){
-			directions.push('north')
-			numMoves++;
-		}
-		if(currentRoom.name === map.rooms[x].west){
-			directions.push('east')
-			numMoves++;
-		}
-		if(currentRoom.name === map.rooms[x].east){
-			directions.push('west')
-			numMoves++;
-		}
-	}
-	if(directions.length === 0){
-	console.log("You have no moves. You are DEAD")
-	rl.close()
-	}else{
-		console.log("You can move " + directions.toString() + '.')
-	}
-};
+	var compass = ['north','south','east','west']
+		compass.forEach(function (way){
+			if(null !== currentRoom[way]){
+				directions.push(way)
+				numMoves++
+			}
+	})
+	console.log("You can move " + directions.toString() + '.')
+}			
 
-function move(currentRoom,move){
-	
-	currentRoom = currentRoom.move
+function move(move){
+	for(var x = 0; x < map.rooms.length; x++){
+		if(map.rooms[x].name === currentRoom[move]){
+			currentRoom = map.rooms[x]
+			break
+		}
+	}
 }
 
-
-
-
-
-
-
+while(currentRoom.treasure === false){
+	console.log("You are in room " + currentRoom.room)
+}
 
 whatsAroundYou(currentRoom);
-move('south')
 console.log(currentRoom.name)
+move("east");
+console.log(currentRoom.name)
+//move('south')
 rl.close();
