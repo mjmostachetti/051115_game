62e050503ef6e3c1a908d7afb337ffce26b67520
Michael Mostachetti <mjmostachetti@gmail.com>
2015-05-12 12:52:06 -0700

    Added fog of war to maze game

--- game/game.js
+++ game/game.js
@@ -1,6 +1,8 @@
 var draw = require('jsi-gamelib').map;
+var newMap = require('jsi-gamelib').map.drawMap;
 var prompt = require('prompt')
 var map = require('./map.json');
+var map2 = require('./map.json');
 var readline = require('readline')
 
 var game = draw(map);
@@ -29,24 +31,29 @@ function move(move){
 	for(var x = 0; x < map.rooms.length; x++){
 		if(map.rooms[x].name === currentRoom[move]){
 			currentRoom = map.rooms[x]
-			console.log("You are in now in room " + currentRoom.name + ".")
+			console.log("You are now in room " + currentRoom.name + ".")
 			break;
 		}
 	}
 }
 
 function play(){
+		//prints new map
+		newMap(map2);
+		//what ways can you move
 		whatsAroundYou(currentRoom)
 		if(currentRoom.treasure !== false){
 			console.log("You've found the treasure!")
 			rl.close()
 		} else{
-			rl.question("You are in room " + currentRoom.name + "\n" +
+			rl.question("You are in room " + currentRoom.name + ".\n" +
 			"What direction do you want to go?", function(direction){
 				console.log("You are moving " + direction + ".");
 				move(direction)
+				currentRoom.visited = true
 				play()
 			})
 		}	
 }
+
 play();
